37ce5dd1e8378c1998e3edc73bed845a7b2b712d
Jenny Chang <cychang428@gmail.com>
2024-11-01 21:05:34 -0700

    update dplyr::

--- R/count_unique.R
+++ R/count_unique.R
@@ -1,5 +1,5 @@
 count_unique <- function(data, summ_var, ..., .groups = "drop") {
   data %>%
-    group_by(...) %>%
-    summarize(unique_sum = n_distinct({{ summ_var }}[!is.na({{ summ_var }})]), .groups = .groups)
+    dplyr::group_by(...) %>%
+    dplyr::summarize(unique_sum = dplyr::n_distinct({{ summ_var }}[!is.na({{ summ_var }})]), .groups = .groups)
 }
